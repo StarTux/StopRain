c8864bbcbd106371645fa9d5e7763db230dc08db
StarTux <ths.loy@googlemail.com>
2022-07-21 10:48:25 +0200

    Strip legacy MiniMap support

--- src/main/java/com/winthier/stoprain/StopRainPlugin.java
+++ src/main/java/com/winthier/stoprain/StopRainPlugin.java
@@ -1,12 +1,10 @@
-package com.winthier.endrain;
+package com.winthier.stoprain;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 import java.util.UUID;
 import org.bukkit.ChatColor;
@@ -18,16 +16,15 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.metadata.FixedMetadataValue;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class StopRainPlugin extends JavaPlugin implements Listener {
-    private Set<UUID> players;
-    private boolean supportMiniMap = true;
+    private final Set<UUID> players = new HashSet<>();
 
     @Override
     public void onEnable() {
         getServer().getPluginManager().registerEvents(this, this);
+        loadPlayers();
         for (Player player: getServer().getOnlinePlayers()) {
             setupPlayer(player);
         }
@@ -43,7 +40,7 @@ public final class StopRainPlugin extends JavaPlugin implements Listener {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (!(sender instanceof Player)) return false;
-        Player player = (Player)sender;
+        Player player = (Player) sender;
         if (args.length == 0) {
             turnPlayer(player, true);
         } else {
@@ -59,21 +56,21 @@ public final class StopRainPlugin extends JavaPlugin implements Listener {
         return true;
     }
 
-    void turnPlayer(Player player, boolean on) {
+    private void turnPlayer(Player player, boolean on) {
         final UUID uuid = player.getUniqueId();
         if (on) {
-            if (getPlayers().contains(uuid)) {
+            if (players.contains(uuid)) {
                 player.setPlayerWeather(WeatherType.CLEAR);
                 player.sendMessage(ChatColor.DARK_AQUA + "Rain is already turned off.");
             } else {
-                getPlayers().add(uuid);
+                players.add(uuid);
                 savePlayers();
                 player.setPlayerWeather(WeatherType.CLEAR);
                 player.sendMessage(ChatColor.DARK_AQUA + "The rain will stop in a jiffy.");
             }
         } else {
-            if (getPlayers().contains(uuid)) {
-                getPlayers().remove(uuid);
+            if (players.contains(uuid)) {
+                players.remove(uuid);
                 savePlayers();
                 player.resetPlayerWeather();
                 player.sendMessage(ChatColor.DARK_AQUA + "Rain turned back on.");
@@ -84,27 +81,24 @@ public final class StopRainPlugin extends JavaPlugin implements Listener {
         }
     }
 
-    File getSaveFile() {
+    private File getSaveFile() {
         getDataFolder().mkdirs();
         return new File(getDataFolder(), "players.yml");
     }
 
-    Set<UUID> getPlayers() {
-        if (players == null) {
-            players = new HashSet<>();
-            YamlConfiguration config = YamlConfiguration.loadConfiguration(getSaveFile());
-            for (String string : config.getStringList("players")) {
-                try {
-                    players.add(UUID.fromString(string));
-                } catch (IllegalArgumentException iae) {
-                    iae.printStackTrace();
-                }
+    private void loadPlayers() {
+        players.clear();
+        YamlConfiguration config = YamlConfiguration.loadConfiguration(getSaveFile());
+        for (String string : config.getStringList("players")) {
+            try {
+                players.add(UUID.fromString(string));
+            } catch (IllegalArgumentException iae) {
+                iae.printStackTrace();
             }
         }
-        return players;
     }
 
-    void savePlayers() {
+    private void savePlayers() {
         if (players == null) return;
         YamlConfiguration config = new YamlConfiguration();
         List<String> list = new ArrayList<>();
@@ -117,35 +111,15 @@ public final class StopRainPlugin extends JavaPlugin implements Listener {
         }
     }
 
-    @EventHandler public void onPlayerJoin(PlayerJoinEvent event) {
+    @EventHandler
+    private void onPlayerJoin(PlayerJoinEvent event) {
         setupPlayer(event.getPlayer());
     }
 
-    void setupPlayer(Player player) {
+    private void setupPlayer(Player player) {
         final UUID uuid = player.getUniqueId();
-        if (getPlayers().contains(uuid)) {
+        if (players.contains(uuid)) {
             player.setPlayerWeather(WeatherType.CLEAR);
-        }
-        if (supportMiniMap) {
-            Map<String, Object> map = new HashMap<>();
-            map.put("Type", "Boolean");
-            map.put("Value", players.contains(uuid));
-            map.put("DisplayName", "Disable Rain");
-            map.put("Priority", 0);
-            Runnable onUpdate = () -> {
-                boolean v = map.get("Value") == Boolean.TRUE;
-                if (v) {
-                    players.add(uuid);
-                    player.setPlayerWeather(WeatherType.CLEAR);
-                } else {
-                    players.remove(uuid);
-                    player.resetPlayerWeather();
-                }
-            };
-            map.put("OnUpdate", onUpdate);
-            List<Map> list = new ArrayList<>();
-            list.add(map);
-            player.setMetadata("MiniMapSettings", new FixedMetadataValue(this, list));
         }
     }
 }
